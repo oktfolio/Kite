07bf815768d4bfd9d83f7b393b7c3bd19eaa5334
Oktfolio <oktfolio@gmail.com>
2020-05-04 21:02:02 +0800

    update

--- src/main/kotlin/com/oktfolio/kite/security/authentication/PhoneAuthenticationToken.kt
+++ src/main/kotlin/com/oktfolio/kite/security/authentication/PhoneAuthenticationToken.kt
@@ -8,7 +8,7 @@ import org.springframework.security.core.GrantedAuthority
  * @date 2020/05/04
  */
 class PhoneAuthenticationToken : AbstractKiteAuthenticationToken {
-    constructor(principal: Any?, credentials: Any?) : super(principal, credentials)
-    constructor(principal: Any?, credentials: Any?, authorities: Collection<GrantedAuthority?>?)
+    constructor(principal: Any, credentials: Any?) : super(principal, credentials)
+    constructor(principal: Any, credentials: Any?, authorities: Collection<GrantedAuthority?>?)
             : super(principal, credentials, authorities)
 }
